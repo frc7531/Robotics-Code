68925afe279e1f2a796bb3b7b11e5b663314c988
orangeisbetter <orangeisbetter@outlook.com>
2022-01-22 10:18:06 -0600

    Update Robot.java
    updated method drive(), now supports dead zone control and square inputs control

--- WestBasic/src/main/java/frc/robot/Robot.java
+++ WestBasic/src/main/java/frc/robot/Robot.java
@@ -34,26 +34,61 @@ public class Robot extends TimedRobot {
   Joystick leftStick = new Joystick(0);
   Joystick rightStick = new Joystick(1);
 
+  private boolean idleMode = true; // false = coast, true = brake
+
   @Override
   public void robotInit() {
     CameraServer.startAutomaticCapture();
+
+    if(idleMode) {
+      //set drive motors to brake when idle
+      leftMotor1.setIdleMode(IdleMode.kBrake);
+      leftMotor2.setIdleMode(IdleMode.kBrake);
+      leftMotor3.setIdleMode(IdleMode.kBrake);
+      rightMotor1.setIdleMode(IdleMode.kBrake);
+      rightMotor2.setIdleMode(IdleMode.kBrake);
+      rightMotor3.setIdleMode(IdleMode.kBrake);
+    } else {
+      //set drive motors to coast when idle
+      leftMotor1.setIdleMode(IdleMode.kCoast);
+      leftMotor2.setIdleMode(IdleMode.kCoast);
+      leftMotor3.setIdleMode(IdleMode.kCoast);
+      rightMotor1.setIdleMode(IdleMode.kCoast);
+      rightMotor2.setIdleMode(IdleMode.kCoast);
+      rightMotor3.setIdleMode(IdleMode.kCoast);
+    }
   }
 
   @Override
   public void teleopPeriodic() {
-    drive(leftStick.getY(), rightStick.getY());
+    drive(leftStick.getY(), rightStick.getY(), true, 20);
   }
+  /**Custom method to drive the robot in tank drive mode, with square input mode*/
+  public void drive(double left_speed, double right_speed, boolean squareInputs, float deadZone) {
+    if(squareInputs) {
+      if((int)(left_speed * deadZone) != 0) {
+        leftMotor1.set(-Math.copySign(left_speed * left_speed, left_speed));
+        leftMotor2.set(-Math.copySign(left_speed * left_speed, left_speed));
+        leftMotor3.set(-Math.copySign(left_speed * left_speed, left_speed));
+      } else { leftMotor1.set(0);  leftMotor2.set(0);  leftMotor3.set(0); }
 
-  public void drive(double left_speed, double right_speed) {
-    if((int)(left_speed * 20) != 0) {
-      leftMotor1.set(-Math.copySign(left_speed * left_speed, left_speed));
-      leftMotor2.set(-Math.copySign(left_speed * left_speed, left_speed));
-      leftMotor3.set(-Math.copySign(left_speed * left_speed, left_speed));
-    } else { leftMotor1.set(0);  leftMotor2.set(0);  leftMotor3.set(0); }
-    if((int)(right_speed * 20) != 0) {
-      rightMotor1.set(Math.copySign(right_speed * right_speed, right_speed));
-      rightMotor2.set(Math.copySign(right_speed * right_speed, right_speed));
-      rightMotor3.set(Math.copySign(right_speed * right_speed, right_speed));
-    } else { rightMotor1.set(0);  rightMotor2.set(0);  rightMotor3.set(0); }
+      if((int)(right_speed * deadZone) != 0) {
+        rightMotor1.set(Math.copySign(right_speed * right_speed, right_speed));
+        rightMotor2.set(Math.copySign(right_speed * right_speed, right_speed));
+        rightMotor3.set(Math.copySign(right_speed * right_speed, right_speed));
+      } else { rightMotor1.set(0);  rightMotor2.set(0);  rightMotor3.set(0); }
+    } else {
+      if((int)(left_speed * deadZone) != 0) {
+        leftMotor1.set(-left_speed);
+        leftMotor2.set(-left_speed);
+        leftMotor3.set(-left_speed);
+      } else { leftMotor1.set(0);  leftMotor2.set(0);  leftMotor3.set(0); }
+
+      if((int)(right_speed * deadZone) != 0) {
+        rightMotor1.set(right_speed);
+        rightMotor2.set(right_speed);
+        rightMotor3.set(right_speed);
+      } else { rightMotor1.set(0);  rightMotor2.set(0);  rightMotor3.set(0); }
+    }
   }
 }
