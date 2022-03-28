72f57da9b9da8ef990a1b281f45005b5b19cbd62
orangeisbetter <69521164+orangeisbetter@users.noreply.github.com>
2022-03-28 17:50:53 -0500

    installed CTRE and REV libraries

--- SwerveDrive2023/src/main/java/frc/robot/Robot.java
+++ SwerveDrive2023/src/main/java/frc/robot/Robot.java
@@ -6,17 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  
   @Override
   public void robotInit() {}
 
