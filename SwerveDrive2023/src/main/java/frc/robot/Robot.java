d5642996e1c796e9ebfc00f56b5bfbe2ee2c29f0
orangeisbetter <69521164+orangeisbetter@users.noreply.github.com>
2022-03-28 19:58:20 -0500

    possibly working swervedrive code (who knows)

--- SwerveDrive2023/src/main/java/frc/robot/Robot.java
+++ SwerveDrive2023/src/main/java/frc/robot/Robot.java
@@ -4,37 +4,89 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.*;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Robot extends TimedRobot {
-  
-  @Override
-  public void robotInit() {}
+  CANSparkMax[] motors;
+
+  Translation2d frontLeftModule = new Translation2d(-5, 5);
+  Translation2d frontRightModule = new Translation2d(5, 5);
+  Translation2d rearRightModule = new Translation2d(5, -5);
+  Translation2d rearLeftModule = new Translation2d(-5, -5);
+  DutyCycleEncoder frontLeftEncoder = new DutyCycleEncoder(0);
+  SwerveDriveKinematics sdk = new SwerveDriveKinematics(frontLeftModule, frontRightModule, rearRightModule, rearLeftModule);
+
+  Joystick controller = new Joystick(0);
 
   @Override
-  public void robotPeriodic() {}
+  public void robotInit() {
+    frontLeftEncoder.setConnectedFrequencyThreshold(900);
+    for(int i = 0; i < 8; i++) {
+      motors[i] = new CANSparkMax(i + 1, MotorType.kBrushless);
+    }
+  }
 
   @Override
-  public void autonomousInit() {}
+  public void robotPeriodic() {
+
+  }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousInit() {
+
+  }
 
   @Override
-  public void teleopInit() {}
+  public void autonomousPeriodic() {
+
+  }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopInit() {
+
+  }
 
   @Override
-  public void disabledInit() {}
+  public void teleopPeriodic() {
+    swerveDrive(new ChassisSpeeds(controller.getRawAxis(0), controller.getRawAxis(1), controller.getRawAxis(2)));
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledInit() {
+
+  }
 
   @Override
-  public void testInit() {}
+  public void disabledPeriodic() {
 
-  @Override
-  public void testPeriodic() {}
+  }
+
+  public void swerveDrive(ChassisSpeeds speeds) {
+    SwerveModuleState[] moduleStates = sdk.toSwerveModuleStates(speeds);
+    for(int i = 0; i < 4; i++) {
+      swerveModuleControl(moduleStates[0], speeds.omegaRadiansPerSecond, i);
+    }
+  }
+
+  public void swerveModuleControl(SwerveModuleState moduleState, double rotationSpeed, int motorNumber) {
+    double speedMotor;
+    double rotationMotor;
+    speedMotor = moduleState.speedMetersPerSecond;
+    if(frontLeftEncoder.get() % 1.0 < (moduleState.angle.getDegrees() - 5) / 360) {
+      rotationMotor = rotationSpeed;
+    } else if(frontLeftEncoder.get() % 1.0 > (moduleState.angle.getDegrees() - 5) / 360) {
+      rotationMotor = -rotationSpeed;
+    } else {
+      rotationMotor = 0;
+    }
+    motors[motorNumber * 2].set(speedMotor + rotationMotor);
+    motors[motorNumber * 2 + 1].set(speedMotor - rotationMotor);
+  }
 }
